0c09cb3906bb102a5a1b9f4c8f3235a58ed1b543
Andy <andy@sharpcloud.com>
2011-06-30 10:04:53 +0100

    added email capability. Will now send an email when a deployment status changes if a valid stmp server account and email address is provided.

--- lib/Program.cs
+++ lib/Program.cs
@@ -8,46 +8,59 @@ using System.IO;
 using System.Xml;
 using System.Xml.Linq;
 using System.Threading;
+using System.Net.Mail;
 
 namespace Notify
 {    
     class Program
     {
-        static Dictionary<string, string> deploymentTracker = new Dictionary<string, string>();
+
+        
+        /// <summary>
+        /// Config settings - please configure the strings below to setup smtp account for email and Windows Azure service to monitor
+        /// </summary>
+        ///smtp host
+        private static string smtpHost = "##mail.smtp.com##";
+        //Username for smtp host
+        private static string username = "##smtpusername##";
+        //Password for smtp host
+        private static string pass = "##passwordd##";
+        //from email address
+        private static string fromemail = "##from@you.com##";
+        //to email address
+        private static string toAddress = "##to@you.com##";
+        //service to monitor
+        private static string serviceName = "yourazuretest";
+        // The ID for the Windows Azure subscription.
+        private static string subscriptionId = "00000000-0000-0000-0000-000000000000";
+        // The thumbprint for the certificate. This certificate would have been
+        // previously added as a management certificate within the Windows Azure management portal.
+        private static string thumbPrint = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+        //The time period to recheck the service status in seconds
+        private static long secondsToCheck = 30;
+        
 
         static void Main(string[] args)
         {
             try
-            {                                
+            {
                 X509Store certStore = null;
                 X509Certificate2Collection certCollection = null;
-                X509Certificate2 certificate = null;                
+                X509Certificate2 certificate = null;
                 Uri requestUri = null;
-
-
-                //service to monitor
-                string serviceName = "sharpcloud-test";
 
                 //service opration
                 string operation = "hostedservices" + "/" + serviceName + "?embed-detail=true";
 
-                // The ID for the Windows Azure subscription.
-                string subscriptionId = "02db5659-617a-445a-8ed3-165bf6e519bb";
+             
 
-                // The thumbprint for the certificate. This certificate would have been
-                // previously added as a management certificate within the Windows Azure management portal.
-                string thumbPrint = "ACFC0FBD3AE4EB6E242B99B82B56DF2D240E37E6";
-
-                //The time period to recheck the service status in seconds
-                long secondsToCheck = 30;
-                
                 certStore = new X509Store(StoreName.My, StoreLocation.CurrentUser);
                 certStore.Open(OpenFlags.ReadOnly);
                 certCollection = certStore.Certificates.Find(
                                      X509FindType.FindByThumbprint,
                                      thumbPrint,
                                      false);
-                
+
                 certStore.Close();
 
                 if (certCollection.Count == 0)
@@ -59,7 +72,7 @@ namespace Notify
                 Console.WriteLine(">> Using certificate with thumbprint: " + thumbPrint);
                 Console.WriteLine(">> Fetching status of service: " + serviceName);
                 Console.WriteLine(">> With subscription id of: " + subscriptionId);
-                Console.WriteLine(">> Checking every " + secondsToCheck + " seconds");
+                Console.WriteLine(">> Checking every " + secondsToCheck + " seconds...starting check");
 
                 requestUri = new Uri("https://management.core.windows.net/"
                                      + subscriptionId
@@ -68,71 +81,117 @@ namespace Notify
 
 
                 Timer timer = new Timer((o) =>
-                {                    
+                {
                     GetStatus(certificate, requestUri);
-                },null,0,secondsToCheck * 1000);
-                
-                
-                
+                }, null, 0, secondsToCheck * 1000);
+
+
+
             }
-            catch (Exception e)
+            catch (Exception ex)
             {
 
-                Console.WriteLine("Error encountered: " + e.Message);
-
+                Console.WriteLine("Error encountered: " + ex.Message);                
+            }
+            finally
+            {
+                Console.ReadLine();
             }
            
-            Console.ReadLine();
+            
         }
 
         private static void GetStatus(X509Certificate2 certificate, Uri requestUri)
-        {                        
-            HttpWebRequest httpWebRequest = (HttpWebRequest)HttpWebRequest.Create(requestUri);
-            httpWebRequest.ClientCertificates.Add(certificate);
-            httpWebRequest.Headers.Add("x-ms-version", "2011-02-25");
-            using (HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse())            
-            using (Stream responseStream = httpWebResponse.GetResponseStream())
-            using (StreamReader reader = new StreamReader(responseStream))
+        {
+            try
             {
-                var result = XDocument.Parse(reader.ReadToEnd());
-
-                var query = from e in result.Elements().Elements()
-                            where e.Name == "{http://schemas.microsoft.com/windowsazure}Deployments"
-                            select e;
-
-                var list = query.ToList();
-
-                list.ForEach(x =>
+                HttpWebRequest httpWebRequest = (HttpWebRequest)HttpWebRequest.Create(requestUri);
+                httpWebRequest.ClientCertificates.Add(certificate);
+                httpWebRequest.Headers.Add("x-ms-version", "2011-02-25");
+                using (HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse())
+                using (Stream responseStream = httpWebResponse.GetResponseStream())
+                using (StreamReader reader = new StreamReader(responseStream))
                 {
-                    var deployments = from e in x.Elements()
-                                      select new Tuple<string, string>((string)e.Element("{http://schemas.microsoft.com/windowsazure}Url"), (string)e.Element("{http://schemas.microsoft.com/windowsazure}Status"));
+                    var result = XDocument.Parse(reader.ReadToEnd());
 
+                    var query = from e in result.Elements().Elements()
+                                where e.Name == "{http://schemas.microsoft.com/windowsazure}Deployments"
+                                select e;
 
-                    deployments.ToList().ForEach(xx =>
+                    var list = query.ToList();
+
+                    var temp = list[0];
+                    list.ForEach(x =>
                     {
-                        if (deploymentTracker.ContainsKey(xx.Item1))
+
+                        var deployments = from e in x.Elements()
+                                          select new Tuple<string, string>((string)e.Element("{http://schemas.microsoft.com/windowsazure}Url"), (string)e.Element("{http://schemas.microsoft.com/windowsazure}Status"));
+
+
+                        deployments.ToList().ForEach(xx =>
                         {
-                            if (deploymentTracker[xx.Item1] != xx.Item2)
+                            if (deploymentTracker.ContainsKey(xx.Item1))
                             {
-                                Console.WriteLine(string.Format("Status has changed for {0} to {1}"));
-                                deploymentTracker[xx.Item1] = xx.Item2;
+                                if (deploymentTracker[xx.Item1] != xx.Item2)
+                                {
+                                    var statusChange = string.Format("Status has changed for {0} from {1} to {2}", xx.Item1, deploymentTracker[xx.Item1], xx.Item2);
+                                    Console.WriteLine(statusChange + ", sending email...");
+                                    deploymentTracker[xx.Item1] = xx.Item2;
+                                    SendEmail(statusChange);
+                                }
+                                else
+                                {
+                                    Console.Write(".");
+                                }
                             }
                             else
                             {
-                                Console.Write(".");
+                                deploymentTracker.Add(xx.Item1, xx.Item2);
+                                Console.WriteLine(string.Format("Tracking {0} with a status of {1}.", xx.Item1, xx.Item2));
                             }
                         }
-                        else
-                        {
-                            deploymentTracker.Add(xx.Item1, xx.Item2);
-                            Console.WriteLine(string.Format("Tracking {0} with a status of {1}.", xx.Item1, xx.Item2));
-                        }
+                        );
+
                     }
                     );
+                }       
+            }
+            catch (Exception ex)
+            {
 
-                }
-                );
-            }               
+                Console.WriteLine("Error encountered: " + ex.Message);
+            }
+                  
         }
+
+
+        private static void SendEmail(string statusChange)
+        {
+            try
+            {
+                using (SmtpClient client = new SmtpClient(smtpHost))
+                {
+                    client.Port = 2525;
+                    client.Credentials = new NetworkCredential(username, pass);
+                    MailAddress from = new MailAddress(fromemail, fromemail,
+                                                       System.Text.Encoding.UTF8);
+                    MailAddress to = new MailAddress(toAddress);
+                    using (MailMessage message = new MailMessage(from, to))
+                    {
+                        message.Subject = statusChange;
+                        message.Body = "";
+                        client.Send(message);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error encountered: " + ex.Message);
+            }
+            
+        }
+
+        //tracks deployments
+        private static Dictionary<string, string> deploymentTracker = new Dictionary<string, string>();
     }
 }
--- readme.txt
+++ readme.txt
@@ -1,1 +1,25 @@
 A simple console app to monitor the status of a Microsoft Azure Service and notify by email when a change occurs.
+To use complete the config section in Program.cs and build in visual studio 2010. You can then run the console app exe.
+
+/// <summary>
+/// Config settings - please configure the strings below to setup smtp account for email and Windows Azure service to monitor
+/// </summary>
+///smtp host
+private static string smtpHost = "##mail.smtp.com##";
+//Username for smtp host
+private static string username = "##smtpusername##";
+//Password for smtp host
+private static string pass = "##passwordd##";
+//from email address
+private static string fromemail = "##from@you.com##";
+//to email address
+private static string toAddress = "##to@you.com##";
+//service to monitor
+private static string serviceName = "yourazuretest";
+// The ID for the Windows Azure subscription.
+private static string subscriptionId = "00000000-0000-0000-0000-000000000000";
+// The thumbprint for the certificate. This certificate would have been
+// previously added as a management certificate within the Windows Azure management portal.
+private static string thumbPrint = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+//The time period to recheck the service status in seconds
+private static long secondsToCheck = 30;
